6c6d9ff657e66ace6b20673330e0367bb5ad8540
kalbuld <jamestudor9@gmail.com>
2024-10-08 21:46:58 +0000

    Added a random line to testpython.py

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # testrepo
 
 It's a markdown file in this repository
+
+#
--- testpython.py
+++ testpython.py
@@ -1,3 +1,5 @@
 #document the code
 #display the output
 print("Yes, what did you expect... This is a new python file")
+
+print("I've made some edits, now trying a push")
